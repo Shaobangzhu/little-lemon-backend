c38ace51e63f202dc2db25bb277779e3ae4e40c1
Chaoran Lu <andyluloveusc@gmail.com>
2024-08-15 19:25:55 -0700

    modify username and pswd for mysql database access

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -81,8 +81,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'LittleLemon',
-        'USER': 'root',
-        'PASSWORD': 'Lcr123lcr',
+        'USER': 'YOUR_USERNAME',
+        'PASSWORD': 'YOUR_PASSWORD',
         'HOST': '127.0.0.1',
         'PORT': '3306',
         'OPTIONS': {
